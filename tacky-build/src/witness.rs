88501cb7b01ddb7cc165e36884e66c5c2e89c12d
VladimirBramstedt <vladimir.bramstedt@imperva.com>
2024-01-12 15:40:39 +0100

    get len of buf

--- tacky-build/src/witness.rs
+++ tacky-build/src/witness.rs
@@ -17,6 +17,9 @@ pub fn message_def_writer(w: &mut Fmter<'_>, name: &str) -> std::fmt::Result {
     indented!(w, r"pub fn new(buf: &'buf mut Vec<u8>, tag: Option<u32>) -> Self {{")?;
     indented!(w, r"    Self {{tack: Tack::new(buf, tag)}}")?;
     indented!(w, r"}}")?;
+    indented!(w, r"pub fn written(&self) -> usize {{")?;
+    indented!(w, r"    self.tack.buffer.len()")?;
+    indented!(w, r"}}")?;
     w.unindent();
     indented!(w, r"}}")?;
     indented!(w)
